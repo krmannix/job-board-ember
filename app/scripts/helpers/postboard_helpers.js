93a429877ebde72bafbca0a2e0edadde965c8f83
Kevin Mannix <kmannix@bu.edu>
2014-12-23 20:45:47 -0500

    Transition handlebar template over to helper

--- app/scripts/helpers/postboard_helpers.js
+++ app/scripts/helpers/postboard_helpers.js
@@ -1,6 +1,26 @@
 Ember.Handlebars.helper('job-posting-postboard', function(value, options) {
 	var job = value;
-	var html = "<li class=\"col-md-4\">" + job.job_title + "</li>";
-	console.log(job.job_title);
+	var types = "";
+	for (var i = 0; i < job.type_of_job.length; i++) {
+		types += "<li>" + job.type_of_job[i] + "</li>";
+	}
+	var html = "<li class=\"col-md-4\">" +
+								"<div class=\"job-post-container\">" +
+									"<div class=\"job-title\">" +
+										job.job_title +
+									"</div>" +
+									"<div class=\"company-title\">" +
+										job.company_name +
+									"</div>" +
+									"<div class=\"description-short\">" +
+										job.description_full +
+									"</div>" +
+									"<div class=\"type-of-job\">" +
+										"<ul>" + 
+										types +
+										"</ul>" +
+									"</div>" +
+								"</div>" +
+							"</li>";
 	return new Handlebars.SafeString(html);
 });
--- app/scripts/routes/postboard_route.js
+++ app/scripts/routes/postboard_route.js
@@ -3,12 +3,11 @@ BuJobBoard.PostboardRoute = Ember.Route.extend({
 		var job_postings = [];
 		for (var i = 0; i < 10; i++) {
 			var k = chance.word();
-			console.log(k + "..");
 			job_postings.push(BuJobBoard.JobPosting.create({
 				job_title: k,
 				company_name: chance.word(),
-				type_of_job: chance.bool() ? 'i' : 'f',
-				full_time: chance.bool(),
+				type_of_job: [chance.string({length: 3}), chance.string({length: 3})],
+				full_time: chance.bool() ? 'i' : 'f',
 				description_full: chance.paragraph({sentences: 1}),
 				contact_email: chance.email({domain: "bu.edu"}), 
 				id: chance.bb_pin()
