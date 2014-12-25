74c60cf7c594b141ed75dbf6a5f1299964607e64
Kevin Mannix <kmannix@bu.edu>
2014-12-25 01:40:18 -0500

    Clicking works,not sure if I should make it a view however

--- app/scripts/controllers/postboard_controller.js
+++ app/scripts/controllers/postboard_controller.js
@@ -3,9 +3,11 @@ BuJobBoard.PostboardController = Ember.ObjectController.extend({
 		sidebarPageClick: function(term) {
 			console.log("Click occured " + term);
 		},
-		addjobclick: function(id) {
+		addJobClick: function(id) {
 			// Should add id to class
-			console.log(id);
+			var job_post = Ember.$("#" + id);
+			if (job_post.hasClass("clicked")) job_post.removeClass("clicked");
+			else job_post.addClass("clicked");
 		}
 	}
 });
