e186b2ac11940a87347e62866ec190aed7e91aae
Kevin Mannix <kmannix@bu.edu>
2014-12-26 00:04:40 -0500

    Figured out some data checking for form fields. Still have a little more

--- app/scripts/controllers/submit_job_controller.js
+++ app/scripts/controllers/submit_job_controller.js
@@ -4,8 +4,30 @@ BuJobBoard.SubmitJobController = Ember.ObjectController.extend({
 	submitEmail: '',
 	submitDescription: '',
 	actions: {
-		job_submit: function() {
-			console.log("SUBMITTED");
+		job_submit: function(schools) {	
+			console.log(this.get("submitCompanyName"));
+			var type_danger = Ember.$('#danger-type');
+			var fields_danger = Ember.$('#danger-fields');
+			var school_danger = Ember.$('#danger-school');
+			type_danger.addClass("hidden"); fields_danger.addClass("hidden"); school_danger.addClass("hidden");
+
+			var full = Ember.$('#full').hasClass("active");
+			var intern = Ember.$('#intern').hasClass("active");
+			var checked_schools = [];
+			for (var i = 0; i < schools.length; i++) {
+				if (Ember.$('#' + schools[i].id).is(':checked')) checked_schools.push(schools[i]);
+			}
+
+			if (!this.get("submitDescription") || !this.get("submitJobTitle") || !this.get("submitEmail") || !this.get("submitCompanyName")) {
+				console.log("WRONG " + this.get("submitDescription"));
+				fields_danger.removeClass("hidden");
+			} else if (!full && !intern) {
+				type_danger.removeClass("hidden");
+			} else if (checked_schools.length === 0) {
+				school_danger.removeClass("hidden");
+			} else {
+
+			}
 		}
 	}
 });
--- app/scripts/routes/submit_job_route.js
+++ app/scripts/routes/submit_job_route.js
@@ -3,7 +3,10 @@ BuJobBoard.SubmitJobRoute = Ember.Route.extend({
 		var page_names = ["Postboard", "Saved Jobs", "Submit a Post"];
 		
 		// Putting all the school info
-		var schools = ["College of Engineering", "School of Management", "College of Communication", "College of Arts & Sciences"];
+		var schools = [{name: "College of Engineering", id: "eng"}, 
+					   {name: "School of Management", id: "smg"},
+					   {name: "College of Communication", id: "com"},
+					   {name: "College of Arts & Sciences", id: "cas"}];
 
 		return {page_names: page_names, schools: schools};
 	}
