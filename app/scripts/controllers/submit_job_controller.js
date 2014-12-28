d06774dbc30dcf17c52d0af1d6f7e77982f37714
Kevin Mannix <kmannix@bu.edu>
2014-12-27 23:57:27 -0500

    Figured out radio buttons. Gonna now work on Node JS

--- app/scripts/controllers/submit_job_controller.js
+++ app/scripts/controllers/submit_job_controller.js
@@ -6,16 +6,16 @@ BuJobBoard.SubmitJobController = Ember.ObjectController.extend({
 	actions: {
 		job_submit: function(schools) {	
 			// GET ALL DATA NEEDED
-			var submit_success = Ember.$("#success-submit");
-			var type_danger = Ember.$('#danger-type');
-			var fields_danger = Ember.$('#danger-fields');
-			var school_danger = Ember.$('#danger-school');
-			var email_danger = Ember.$("#danger-email");
+			var submit_success = Ember.$("#success-submit"),
+				type_danger = Ember.$('#danger-type'),
+				fields_danger = Ember.$('#danger-fields'),
+				school_danger = Ember.$('#danger-school'),
+				email_danger = Ember.$("#danger-email");
 			type_danger.addClass("hidden"); fields_danger.addClass("hidden"); 
 			school_danger.addClass("hidden"); email_danger.addClass("hidden"); submit_success.addClass("hidden");
-			var full = Ember.$('#full').hasClass("active");
-			var intern = Ember.$('#intern').hasClass("active");
-			var checked_schools = [];
+			var full = Ember.$('#full').hasClass("active"),
+				intern = Ember.$('#intern').hasClass("active"),
+				checked_schools = [];
 			for (var i = 0; i < schools.length; i++) {
 				if (Ember.$('#' + schools[i].id).is(':checked')) checked_schools.push(schools[i]);
 			}
