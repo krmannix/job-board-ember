cfbf827de3110c08b7cedee9d38d0e1013d8b07e
Kevin Mannix <kmannix@bu.edu>
2015-02-01 15:40:34 -0500

    Behavior added for student signup validation

--- app/scripts/controllers/index_controller.js
+++ app/scripts/controllers/index_controller.js
@@ -81,9 +81,47 @@ BuJobBoard.IndexController = Ember.ObjectController.extend({
 		},
 		signupSubmitStudent: function() {
 
+			var self = this;
+			if (validateFields()) {
+				var user = new Parse.User();
+				user.set('username', this.get('emailStudent'));
+				user.set('password', this.get('passwordStudent'));
+				user.set('firstName', this.get('firstNameStudent'));
+				user.set('lastName', this.get('lastNameStudent'));
+				user.set('buid', this.get('idStudent'));
+				user.signUp(null, {
+					success: function(user_) {
+						Ember.$('#signup-student').modal('hide');
+						this.set('controllers.application.currentUser', user_);
+						Ember.$('#log-out-button').removeClass('hidden');
+						self.transitionToRoute('postboard');
+					}, error: function(user_, error) {
+						// If it does, show error message
+						console.log(error.code);
+						self.set("errorMessageStudent", error.message);
+					}
+				});
+			}
+
 			// Check all fields
 			function validateFields() {
+				if (!this.get('emailStudent')) this.set('errorMessageStudent', 'Please enter your email');
+				else if (!this.get('firstNameStudent')) this.set('errorMessageStudent', 'Please enter your first name');
+				else if (!this.get('lastNameStudent')) this.set('errorMessageStudent', 'Please enter your last name');
+				else if (!this.get('idStudent')) this.set('errorMessageStudent', 'Please enter your BU ID');
+				else if (!this.get('passwordStudent')) this.set('errorMessageStudent', 'Please enter a password');
+				else if (!this.get('confirmStudent')) this.set('errorMessageStudent', 'Please confirm your password');
+				else if (this.get('passwordStudent') !== this.get('confirmStudent')) this.set('errorMessageStudent', 'Passwords do not match');
+				else if (!isBUID(this.get('idStudent'))) this.set('errorMessageStudent', 'Not a valid BU ID');
+				else {
+					this.set('errorMessageStudent', '');
+					return true;
+				}
+			};
 
+			function isBUID(s) {
+			   var regexp = /[U]\d{8}/
+			   return regexp.test(s);
 			};
 		},
 		login: function() {
@@ -104,14 +142,14 @@ BuJobBoard.IndexController = Ember.ObjectController.extend({
 			Parse.User.logIn(this.get('userEmail'), this.get('userId'), {
 				success: function(user) {
 					// enter page
-					// this.set('controllers.application.currentUser', user);
+					this.set('controllers.application.currentUser', user);
 					Ember.$('#log-out-button').removeClass('hidden');
 					self.transitionToRoute('postboard');
 				}, error: function(user, error) {
 					// If it does, show error message
 					switch(error.code) {
 						case 101:
-							self.set("errorMessage", "Email/BU ID combination does not exist yet. Please sign up.");
+							self.set("errorMessage", "Email/password combination does not exist yet. Please sign up.");
 							break;
 						default:
 							self.set("errorMessage", error.message);
