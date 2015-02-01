8093f71ed8e37d50c86a360dd5fae746945e828d
Kevin Mannix <kmannix@bu.edu>
2015-02-01 15:43:06 -0500

    Amended login functions

--- app/scripts/controllers/index_controller.js
+++ app/scripts/controllers/index_controller.js
@@ -125,38 +125,28 @@ BuJobBoard.IndexController = Ember.ObjectController.extend({
 			};
 		},
 		login: function() {
-			// if (this.get('userEmail') === '' && this.get('userId') === '') {
-			// 	this.set("errorMessage", "Please enter your information");
-			// } else if (this.get('userEmail') === '') {
-			// 	this.set("errorMessage", "Please enter your BU email");
-			// } else if (this.get('userId') === '') {
-			// 	this.set("errorMessage", "Please enter your BU ID #");
-			// } else if (this.get('userEmail').indexOf("@bu.edu") === -1) {
-			// 	this.set("errorMessage", "Please enter your BU email");				
-			// } else {
-			// 	this.set("errorMessage", "");
-			// 	this.transitionToRoute('postboard');
-			// }
-			// Check if user is stored in database
 			var self = this;
-			Parse.User.logIn(this.get('userEmail'), this.get('userId'), {
-				success: function(user) {
-					// enter page
-					this.set('controllers.application.currentUser', user);
-					Ember.$('#log-out-button').removeClass('hidden');
-					self.transitionToRoute('postboard');
-				}, error: function(user, error) {
-					// If it does, show error message
-					switch(error.code) {
-						case 101:
-							self.set("errorMessage", "Email/password combination does not exist yet. Please sign up.");
-							break;
-						default:
-							self.set("errorMessage", error.message);
-							break;
+			if (!this.get('loginEmail')) this.set('errorMessageLogin', 'Please enter your email');
+			else if (!this.get('loginPassword')) this.set('errorMessageLogin', 'Please enter your password');
+			else {
+				// Check if user is stored in database
+				Parse.User.logIn(this.get('loginEmail'), this.get('loginPassword'), {
+					success: function(user) {
+						self.set('controllers.application.currentUser', user);
+						Ember.$('#log-out-button').removeClass('hidden');
+						self.transitionToRoute('postboard');
+					}, error: function(user, error) {
+						switch(error.code) {
+							case 101:
+								self.set("errorMessageLogin", "Email/password combination does not exist yet. Please sign up.");
+								break;
+							default:
+								self.set("errorMessageLogin", error.message);
+								break;
+						}
 					}
-				}
-			});
+				});
+			}
 		}
 	}
 });
